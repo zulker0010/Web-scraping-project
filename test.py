6f6ad9d0c0aab3492d91c1cbd0862f609d5a597b
Zulker Yusuf <zulkeryusuf@gmail.com>
2024-08-29 03:23:23 +0600

    reached impase, cannot parse search result container

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.linkedin.com/search/results/companies/?keywords=Bangladesh&origin=GLOBAL_SEARCH_HEADER&sid=.~o')
-print(r)
-print(r.content)
+r = requests.get('https://www.linkedin.com/search/results/companies/?keywords=bangladesh&origin=CLUSTER_EXPANSION&sid=dzj')
+soup =  BeautifulSoup(r.content, 'html.parser')
+ 
+s = soup.find('ul', class_ = 'reusable-search__entity-result-list list-style-none')
+content = s.find_all('li')
+print(content)
 
-soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.prettify())
+
